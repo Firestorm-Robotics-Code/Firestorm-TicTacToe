c713952d92fcc5b9bda0edd7787cd0028529340e
ArisenDiamond81 <75395318+ArisenDiamond81@users.noreply.github.com>
2020-12-02 16:47:33 -0500

    Create Sampletest.ino

---
+++ samples/bigmotor/Sampletest.ino
@@ -0,0 +1,24 @@
+#include <AccelStepper.h>
+
+//AccelStepper Xaxis(1, 2, 5); // pin 2 = step, pin 5 = direction
+//AccelStepper Yaxis(1, 3, 6); // pin 3 = step, pin 6 = direction
+//AccelStepper Zaxis(1, 4, 7); // pin 4 = step, pin 7 = direction
+
+AccelStepper Xaxis(1, 4, 11); // pin 3 = step, pin 6 = direction
+//AccelStepper Yaxis(1, 4, 7); // pin 4 = step, pin 7 = direction
+//AccelStepper Zaxis(1, 5, 9); // pin 5 = step, pin 8 = direction
+
+void setup() {
+  Xaxis.setMaxSpeed(400);
+// Yaxis.setMaxSpeed(400);
+//  Zaxis.setMaxSpeed(400);
+  Xaxis.setSpeed(45);
+//  Yaxis.setSpeed(25);
+//  Zaxis.setSpeed(80);
+}
+
+void loop() {  
+   Xaxis.runSpeed();
+ //  Yaxis.runSpeed();
+ //  Zaxis.runSpeed();
+}
