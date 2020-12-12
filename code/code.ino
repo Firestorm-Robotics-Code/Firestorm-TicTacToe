00d2a016166373bd033512a1a91484cba6dcb117
you-dont-ThinkPad <none>
2020-12-12 16:27:40 -0500

    more to joystick

--- code/code.ino
+++ code/code.ino
@@ -2,30 +2,20 @@
 
 #include "constants.h"
 
+#include "joystick.cpp"
+
 String inches = "0";
 
 Motor motor(X_PUL, X_DIR);
+int[] directions = {0,0,0,0};
 
 void setup(){
   pinMode(TRIGGERPIN,INPUT);
   motor.setSpeed(1600); // 20 inches per second. It doesn't actually go that fast (sadly) but its good enough.
   Serial.begin(9600);
 }
-
-void moveUp(){
-  Serial.println("Up");
-}
-void moveDown(){
-  Serial.println("Down");
-}
-void moveLeft(){
-  Serial.println("Left");
-}
-void moveRight(){
-  Serial.println("Right");
-}
-
 void loop(){
+  joystickRead(*directions);
   if (Serial.available()){
     inches = Serial.readString();
     motor.move(inches.toInt() * 8000);
--- code/joystick.cpp
+++ code/joystick.cpp
@@ -8,22 +8,23 @@ int yPinV = 0
 
 #define vaule 100
 
-void readpin(){
+void readPin(){
   xPinV = analogRead(X_PIN)-512; // zero out what the pin is reading
   yPinV = analogRead(Y_PIN)-512; // same here but for y
 }
 
-void read(){
+void joystickRead(int *directions){
+  readPin();
   if(yPinV >= 480 && (xPinV <= vaule && xPinV >= -vaule)){ // reads if the y pin's vaule is higher than 480 and if the x pin is between vaule and -vaule
-    moveUp();
+    directions[0]=(true);
   }
   else if(yPinV <= -480 && (xPinV <= vaule && xPin >= -vaule)){ // reads if the y pin's vaule is lower than -480 and if the x pin is between vaule and -vaule
-    moveDown();
+    directions[1]=(true);
   }
   else if(xPinV <= 480 && (yPinV <= vaule && yPin >= -vaule)){ // reads if the x pin vaule is higher that 480 and if the y pin is between vaule and -vaule
-    moveRight();
+    directions[2]=(true);
   }
   else if(xPinV <= 480 && (yPinV <= vaule && yPin >= -vaule)){ // reads if the x pin vaule is lower that -480 and if the y pin is between vaule and -vaule
-    moveLeft();
+    directions[3]=(true);
   }
 }
