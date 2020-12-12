c962be8553ef8b078640428170595767f90b7300
LinuxRocks2000 <LinuxRocks2000>
2020-12-12 15:19:00 -0500

    Expose helper functions in code.ino

--- code/code.ino
+++ code/code.ino
@@ -7,13 +7,31 @@ String inches = "0";
 Motor motor(X_PUL, X_DIR);
 
 void setup(){
-  motor.setSpeed(160000); // 20 inches per second. It doesn't actually go that fast (sadly) but its good enough.
+  pinMode(TRIGGERPIN,INPUT);
+  motor.setSpeed(1600); // 20 inches per second. It doesn't actually go that fast (sadly) but its good enough.
   Serial.begin(9600);
 }
+
+void moveUp(){
+  Serial.println("Up");
+}
+void moveDown(){
+  Serial.println("Down");
+}
+void moveLeft(){
+  Serial.println("Left");
+}
+void moveRight(){
+  Serial.println("Right");
+}
+
 void loop(){
   if (Serial.available()){
     inches = Serial.readString();
     motor.move(inches.toInt() * 8000);
   }
-  motor.run();
+  Serial.println(digitalRead(TRIGGERPIN));
+  if (digitalRead(TRIGGERPIN)){
+    motor.run();
+  }
 }
