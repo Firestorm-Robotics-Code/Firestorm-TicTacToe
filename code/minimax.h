2a1a77abacaa4e5b6ed50b663a4d5047d2a87ba0
LinuxRocks2000 <LinuxRocks2000>
2021-02-09 13:02:12 -0500

    Added a new class (experimental, unfinished) to grid.h, added a new Arduino project for Jakson (nano), changed 'code' to 'P1AM' and moved all arduino stuff into new folder 'src'.

--- code/minimax.h
+++
@@ -1,3 +0,0 @@
-/* By Lawson Brashear with help from Tyler Clarke
- * Code for the AI, which can be used for one-player or zero-player mode
- */
---
+++ src/controlpanel_nano/controlpanel_nano.ino
@@ -0,0 +1,7 @@
+void setup(){ /* Setup code here. */
+  
+}
+
+void loop(){ /* This is the mainloop. */
+  
+}
